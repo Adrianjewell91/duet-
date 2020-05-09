b9560df626a788ca8188ed7bb3e3145337906942
Adrian Jewell <adrianjewell91@gmail.com>
2020-05-09 18:30:06 -0400

    messed around, session in same static file

--- public/scripts.js
+++ public/scripts.js
@@ -1,92 +1,97 @@
-var canvas = document.getElementById("canvas");
-var KEYS_PLAYED = new Set([]);
-DrawKeyboard(canvas, KEYS_PLAYED);	
+function run() {
+    var canvas = document.getElementById("canvas");
+    var KEYS_PLAYED = new Set([]);
+    DrawKeyboard(canvas, KEYS_PLAYED);
 
-document.querySelector('#play').addEventListener('click', () => {
-    [audioCtx, keys, websocket] = setUp();
-    let device;
-    window.navigator.requestMIDIAccess().then(access => {
-        device = access.inputs.values().next().value;
-        device.onmidimessage = (e) => _onmidimessage(e, keys, audioCtx, websocket);
+    document.querySelector('#play').addEventListener('click', () => {
+        [audioCtx, keys, websocket] = setUp();
+        let device;
+        window.navigator.requestMIDIAccess().then(access => {
+            device = access.inputs.values().next().value;
+            device.onmidimessage = (e) => {
+                // websocket.emit('note', JSON.stringify(e.data));
+                websocket.send(JSON.stringify(e.data));
+                _onmidimessage(e, keys, audioCtx, websocket);
+            }
+        });
+        disableUI();
+        console.log('playing')
     });
-    disableUI();
-    console.log('playing')
-});
 
-document.querySelector('#listen').addEventListener('click', () => {
-    [audioCtx, keys, websocket] = setUp();
+    document.querySelector('#listen').addEventListener('click', () => {
+        [audioCtx, keys, websocket] = setUp();
 
-    websocket.onmessage = function (event) {
-        console.log(event);
-        payload = {
-            data: JSON.parse(event.data)
+        websocket.onmessage = function (event) {
+            console.log(event);
+            payload = {
+                data: JSON.parse(event.data)
+            }
+            _onmidimessage(payload, keys, audioCtx);
+        };
+        disableUI();
+        console.log('listening')
+    });
+
+    function setUp() {
+        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+        var keys = createKeyBoard(audioCtx);
+        var HOST = location.origin.replace(/^http/, 'ws');
+        // let socket = io();
+        var socket = new WebSocket(HOST);
+
+        console.log(socket);
+
+        return [audioCtx, keys, socket];
+    }
+
+    function disableUI() {
+        document.querySelector('#listen').setAttribute('disabled', true);
+        document.querySelector('#play').setAttribute('disabled', true);
+    }
+
+    function createKeyBoard(audioCtx) {
+        function _mtof(note) {
+            return 440 * Math.pow(2, (note - 69) / 12);
         }
-        _onmidimessage(payload, keys, audioCtx, websocket);
-    };
-    disableUI();
-    console.log('listening')
-});
 
-function setUp() {
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    var keys = createKeyBoard(audioCtx);
-    var HOST = location.origin.replace(/^http/, 'ws');
-    var websocket = new WebSocket(HOST);
+        var keys = new Array(88).fill((idx) => {
+            var oscillator = audioCtx.createOscillator()
+            var gainNode = audioCtx.createGain();
+            oscillator.frequency.value = _mtof(idx);
+            oscillator.connect(gainNode);
+            gainNode.connect(audioCtx.destination);
+            gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
+            oscillator.start();
+            return gainNode;
+        });
 
-    console.log(websocket);
+        // may want to pass all 88 keys through a gain node that levels the volume.
+        // then connect that gain node to the destination (which is the speakers)
 
-    return [audioCtx, keys, websocket];
-}
-
-function disableUI() {
-    document.querySelector('#listen').setAttribute('disabled', true);
-    document.querySelector('#play').setAttribute('disabled', true);
-}
-
-function createKeyBoard(audioCtx) {
-    function _mtof(note) {
-        return 440 * Math.pow(2, (note - 69) / 12);
+        return keys.map((f, idx) => f(idx + 21));
     }
 
-    var keys = new Array(88).fill((idx) => {
-        var oscillator = audioCtx.createOscillator()
-        var gainNode = audioCtx.createGain();
-        oscillator.frequency.value = _mtof(idx);
-        oscillator.connect(gainNode);
-        gainNode.connect(audioCtx.destination);
-        gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
-        oscillator.start();
-        return gainNode;
-    });
+    function _onmidimessage(e, keys, audioCtx) {
+        /**
+        * e.data is an array
+        * e.data[0] = on (144) / off (128) / detune (224)
+        * e.data[1] = midi note
+        * e.data[2] = velocity || detune
+        */
+        switch (e.data[0]) {
+            case 144:
+                KEYS_PLAYED.add(e.data[1] - 21)
+                keys[e.data[1] - 21].gain.setValueAtTime(e.data[2] / 100, audioCtx.currentTime);
+                console.log(e);
+                break;
+            case 128:
+                KEYS_PLAYED.delete(e.data[1] - 21)
+                console.log(e);
+                keys[e.data[1] - 21].gain.setValueAtTime(0, audioCtx.currentTime);
+                break;
+        }
 
-    // may want to pass all 88 keys through a gain node that levels the volume.
-    // then connect that gain node to the destination (which is the speakers)
-
-    return keys.map((f, idx) => f(idx + 21));
-}
-
-function _onmidimessage(e, keys, audioCtx, websocket) {
-    /**
-    * e.data is an array
-    * e.data[0] = on (144) / off (128) / detune (224)
-    * e.data[1] = midi note
-    * e.data[2] = velocity || detune
-    */
-    switch (e.data[0]) {
-        case 144:
-            KEYS_PLAYED.add(e.data[1] - 21)
-            websocket.send(JSON.stringify(e.data));
-            keys[e.data[1] - 21].gain.setValueAtTime(e.data[2] / 100, audioCtx.currentTime);
-            console.log(e);
-            break;
-        case 128:
-            KEYS_PLAYED.delete(e.data[1] - 21)
-            console.log(e);
-            websocket.send(JSON.stringify(e.data));
-            keys[e.data[1] - 21].gain.setValueAtTime(0, audioCtx.currentTime);
-            break;
+        DrawKeyboard(canvas, KEYS_PLAYED)
+        return 0;
     }
-
-    DrawKeyboard(canvas, KEYS_PLAYED)
-    return 0;
 }
--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-'use strict'
+'use strict';
+
 const express = require('express');
-var path = require('path');
+const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
 
-const WebSocket = require('ws');
-
-const server = express();
-server.use(express.static('public'));
-
-server.get('/', function (_, res) {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-server.get('/jam', function (_, res) {
-    res.sendFile(path.join(__dirname + '/public/jam.html'));
-});
-
-server.listen(PORT, () => console.log(`Duet app listening at http://localhost:${PORT}`))
-
+const server = express().use(express.static('public'))
+    .listen(PORT, () => {
+        console.log(`Example app listening on PORT ${PORT}!`)
+    });
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
-    console.log(
-        'Client Connected'
-    );
-
+    console.log(ws);
     ws.on('message', function incoming(message) {
         console.log('received message: %s', message);
         wss.clients.forEach(function each(client) {
@@ -35,6 +22,4 @@ wss.on('connection', function connection(ws) {
         });
 
     });
-
-    ws.on('close', () => console.log('Client disconnected'));
 });

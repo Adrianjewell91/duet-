0086bcce17bfa950c3f8bbde4c8cb73246bbccb5
Adrian Jewell <adrianjewell91@gmail.com>
2020-05-13 14:14:57 -0400

    fix conflicts

--- public/scripts/run.js
+++ public/scripts/run.js
@@ -7,10 +7,7 @@ function run(DrawKeyboard = () => {}) {
     var keys = createKeyBoard(audioCtx);
     
     // now create 88 websockets and see what happens:
-<<<<<<< HEAD
     // var websocket = new WebSocket(location.origin.replace(/^http/, 'ws') + "/0");
-=======
->>>>>>> a1584e6832f59735b497000ca303a1cdb1c1069b
     var sockets = new Array(88).fill((idx) => {
         const websocket = new WebSocket(location.origin.replace(/^http/, 'ws') + "/" + idx);
         websocket.binaryType = 'arraybuffer';
@@ -19,21 +16,13 @@ function run(DrawKeyboard = () => {}) {
             console.log(event);
             payload = new Uint8Array(event.data);
 
-<<<<<<< HEAD
             _onmidimessage(payload, keys, audioCtx, KEYS_PLAYED, (canvas, keysPlayed) => DrawKeyboard(canvas, keysPlayed));
-=======
-            _onmidimessage(payload, keys, audioCtx, KEYS_PLAYED, (a, b) => DrawKeyboard(a, b));
->>>>>>> a1584e6832f59735b497000ca303a1cdb1c1069b
         };
 
         return websocket;
     });
 
     sockets = sockets.map((connect, idx) => connect(idx));
-<<<<<<< HEAD
-=======
-    // var websocket = new WebSocket(location.origin.replace(/^http/, 'ws') + "/0");
->>>>>>> a1584e6832f59735b497000ca303a1cdb1c1069b
 
     window.navigator.requestMIDIAccess().then(access => {
         let activity;
@@ -45,10 +34,7 @@ function run(DrawKeyboard = () => {}) {
 
             access.inputs.values().next().value.onmidimessage = (e) => {
                 console.log(e);
-<<<<<<< HEAD
                 // websocket.send(e.data);
-=======
->>>>>>> a1584e6832f59735b497000ca303a1cdb1c1069b
                 sockets[e.data[1] - 21].send(e.data);
                 _onmidimessage(e.data, keys, audioCtx, KEYS_PLAYED, (a, b) => DrawKeyboard(a, b));
             }
@@ -62,7 +48,6 @@ function run(DrawKeyboard = () => {}) {
 
         console.log(activity);
     });
-<<<<<<< HEAD
 
     // websocket.onmessage = function (event) {
     //     console.log(event);
@@ -70,6 +55,5 @@ function run(DrawKeyboard = () => {}) {
 
     //     _onmidimessage(payload, keys, audioCtx, KEYS_PLAYED, (a, b) => DrawKeyboard(a, b));
     // };
-=======
->>>>>>> a1584e6832f59735b497000ca303a1cdb1c1069b
+    
 }

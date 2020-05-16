a4b592712e3cc5b169ea62e951984bf54f0e9f64
Adrian Jewell <adrianjewell91@gmail.com>
2020-05-16 00:42:26 -0400

    decouple socket server

--- README.md
+++ README.md
@@ -2,12 +2,6 @@ TODO:
 
 Set up a multi instrument calibration - have the user set up a function that determines noteOn notes off logic. 
 
-Red note for player and blue note for messages;
-    - I can keep track of the keys played.
-    - and I can keep track of to whom the notes belong. 
-    - start with just 1 and 2 
-    - when I add notes, add to the NoteOwner set 
-    - pass note owners to the render function and 
 
 This weekend: Sessions:
 Host another web socket-only server on heroku and try to connect to it. 
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 'use strict';
 
 const http = require('http');
-const WebSocket = require('ws');
-const url = require('url');
 const PORT = process.env.PORT || 3000;
 
 const express = require('express');
@@ -10,57 +8,5 @@ const express = require('express');
 const app = express().use(express.static('public'))
 
 const server = http.createServer(app);
-
-//88 web sockets; 
-var keys = new Array(88).fill(() => new WebSocket.Server({ noServer: true }));
-
-keys = keys.map((newSocket) => newSocket());
-
-keys.forEach((socket, idx) => {
-    socket.on('connection', function connection(ws) {
-        //set up the ping
-        var id = setInterval(function () {
-            ws.send(null, function () { })
-        }, 1000);
-        // ...
-        console.log("connected", idx);
-        ws.on('message', function incoming(message) {
-            console.log(`received message: ${message} on PORT: ${PORT}`);
-            socket.clients.forEach(function each(client) {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(message);
-                }
-            });
-
-        });
-
-        // clear the ping when the socket closes
-        ws.on('close', function close() {
-            clearInterval(id);
-        });
-    });
-});
-
-// The initializers 
-var upgradeHandler = {};
-for (let i = 0; i<88; i++) {
-    upgradeHandler[`/${i}`] = (request, socket, head) => {
-        console.log(i);
-        keys[i].handleUpgrade(request, socket, head, function done(ws) {
-            keys[i].emit('connection', ws, request);
-            //wow that shit actually worked.
-            console.log(i);
-        });
-    }
-}
-
-server.on('upgrade', function upgrade(request, socket, head) {
-    const pathname = url.parse(request.url).pathname;
-        if (!upgradeHandler[pathname]) {
-            socket.destroy();
-        } else {
-            upgradeHandler[pathname](request, socket, head);
-        }
-});
 
 server.listen(PORT);
